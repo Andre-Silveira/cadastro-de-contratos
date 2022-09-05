16d521b20df966d3e240cad36e344dc94498f748
Andre.Silveira <andre.inacio@tcsindustrial.com.br>
2022-09-04 22:39:17 -0300

    API dev-week-dio

---
+++ src/Controllers/PessoaController.cs
@@ -0,0 +1,98 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+using src.Models;
+using src.Persistence;
+
+namespace src.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PessoaController : ControllerBase{
+
+    private DatabaseContext _context { get; set; }
+
+    public PessoaController(DatabaseContext context)
+    {
+        this._context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<List<Pessoa>> Get(){
+        var result =_context.Pessoas.Include(p => p.Contratos).ToList();
+        if(!result.Any())
+            return NoContent();
+
+        return Ok(result); 
+    }
+
+    [HttpPost]
+    public ActionResult<Object> Post([FromBody]Pessoa pessoa){
+        try
+        {
+            _context.Pessoas.Add(pessoa);
+            _context.SaveChanges();
+
+            return Created("Criado", pessoa);
+        }
+        catch (System.Exception)
+        {
+            return BadRequest();
+        }
+    }
+
+
+    [HttpPut("{id}")]
+    public ActionResult<Object> Update([FromRoute]int id, [FromBody]Pessoa pessoa){
+        var result = _context.Pessoas.SingleOrDefault(e => e.Id == id);
+
+        if(result is null)
+            return NotFound(new{
+                msg = "Registro não encontrado",
+                status = HttpStatusCode.NotFound
+
+            });
+        
+        try
+        {
+             _context.Pessoas.Update(pessoa);
+             _context.SaveChanges();
+
+            return Ok(new{
+                msg = $"Dados do id {id} atualizados",
+                status = HttpStatusCode.OK
+            });
+        }
+        catch (System.Exception)
+        {
+             return BadRequest(new{
+                msg = $"Ocorreu algum erro ao enviar a solicitação de atualização do id {id}",
+                status = HttpStatusCode.BadRequest
+            });
+        }
+       
+
+      
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult<Object> Delete([FromRoute]int id){
+        
+        var result = _context.Pessoas.SingleOrDefault(e => e.Id == id);
+
+        if(result is null)
+            return BadRequest(new{
+                msg = "Conteúdo inexistente, solicitação inválida"
+                ,
+            status = HttpStatusCode.BadRequest
+            });
+
+        _context.Pessoas.Remove(result);
+        _context.SaveChanges();
+        return Ok(new{
+            msg = $"Dados do id {id} foram deletados.",
+            status = HttpStatusCode.OK
+        });
+    }
+}
